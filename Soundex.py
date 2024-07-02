9b3ad76b4ed658a28349111719dc51835e607ca9
Mohini-1998 <mohinimahalle18@gmail.com>
2024-07-02 10:30:14 +0530

    Update Soundex.py
    reduce complexity

--- Soundex.py
+++ Soundex.py
@@ -19,13 +19,12 @@ def generate_soundex(name):
     soundex = name[0].upper()
     prev_code = get_soundex_code(soundex)
 
-    for char in name[1:]:
+    for char in name[1:4]:
         code = get_soundex_code(char)
         if code != '0' and code != prev_code:
             soundex += code
             prev_code = code
-        if len(soundex) == 4:
-            break
+
 
     # Pad with zeros if necessary
     soundex = soundex.ljust(4, '0')
