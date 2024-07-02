59d3eed4326229c460ba592d565b9e242697cf2b
Mohini-1998 <mohinimahalle18@gmail.com>
2024-07-02 10:19:49 +0530

    Update Soundex.test.py
    four character tested

--- Soundex.test.py
+++ Soundex.test.py
@@ -9,8 +9,29 @@ class TestSoundex(unittest.TestCase):
     def test_single_character(self):
         self.assertEqual(generate_soundex("A"), "A000")
         
-    def test_single_digit(self):
-        self.assertEqual(generate_soundex("1"), "1")
-    
+    def test_four_character(self):
+        self.assertEqual(generate_soundex("BFPV"), "B000")
+        
+    def test_four_character(self):
+        self.assertEqual(generate_soundex("BCDL"), "B234")
+        
+    def test_four_character(self):
+        self.assertEqual(generate_soundex("FGTM"), "F235")
+        
+    def test_four_character(self):
+        self.assertEqual(generate_soundex("PJNR"), "P256") 
+        
+    def test_four_character(self):
+        self.assertEqual(generate_soundex("VKQS"), "V200")
+        
+    def test_four_character(self):
+        self.assertEqual(generate_soundex("XZMN"), "X500")
+        
+    def test_four_character(self):
+        self.assertEqual(generate_soundex("BCDLM"), "B234")
+        
+    def test_four_character(self):
+        self.assertEqual(generate_soundex("BDLMNR"), "B345") 
+        
 if __name__ == '__main__':
     unittest.main()
