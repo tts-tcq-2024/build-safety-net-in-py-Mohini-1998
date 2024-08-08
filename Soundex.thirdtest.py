b19e8abef2871d333852f44fe90cd869f6835b9d
Mohini-1998 <mohinimahalle18@gmail.com>
2024-08-08 12:29:42 +0530

    Update Soundex.thirdtest.py

--- Soundex.thirdtest.py
+++ Soundex.thirdtest.py
@@ -6,8 +6,8 @@ class TestSoundex(unittest.TestCase):
     def test_four_character(self):
         self.assertEqual(generate_soundex("FGTM"), "F235")
         
-    def test_four_character(self):
-        self.assertEqual(generate_soundex("PJNR"), "P256") 
+    #def test_four_character(self):
+        #self.assertEqual(generate_soundex("PJNR"), "P256") 
             
 if __name__ == '__main__':
     unittest.main()
