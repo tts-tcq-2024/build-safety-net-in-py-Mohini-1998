50bf518caa1e51d40e40474b1e792f3786e8dee1
Mohini-1998 <mohinimahalle18@gmail.com>
2024-08-08 12:36:15 +0530

    Update Soundex.py

--- Soundex.py
+++ Soundex.py
@@ -8,26 +8,31 @@ def get_soundex_code(c):
         'M': '5', 'N': '5',
         'R': '6'
     }
-    return mapping.get(c,'0')  # Default to '0' for non-mapped characters
- 
- 
- 
+    return mapping.get(c, '0')  # Default to '0' for non-mapped characters
+
+def truncate(soundex):
+    if len(soundex)>=4:
+        soundex=soundex[0:4]
+    else:
+        soundex=soundex.ljust(4,'0')
+    return soundex
+
+def set_soundex_char(code,prev_code,soundex):
+    if code!='0' and code !=prev_code:
+        soundex += code
+        prev_code = code
+    return soundex,prev_code
+
 def generate_soundex(name):
     if not name:
-       return ""
- 
+        return ""
+
     # Start with the first letter (capitalized)
     soundex = name[0].upper()
     prev_code = get_soundex_code(soundex)
- 
-    for char in name[1:4]:
-        code = get_soundex_code(char)
-        if code != '0' and code != prev_code:
-            soundex += code
-            prev_code = code
 
- 
-    # Pad with zeros if necessary
-    soundex = soundex.ljust(4, '0')
- 
+    for char in name[1:]:
+        code = get_soundex_code(char)
+        soundex,prev_code=set_soundex_char(code,prev_code,soundex)            
+    soundex=truncate(soundex)
     return soundex
